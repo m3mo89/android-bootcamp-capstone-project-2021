0c03947c50931275593dfd342d17898f8bec3ac4
Guillermo Tinoco <m3mo89@gmail.com>
2021-11-01 13:36:04 -0600

    Add AvailableBookResponse model

---
+++ app/src/main/java/com/wizeline/bootcamp/capstone/data/mock/AvailableBookResponse.kt
@@ -0,0 +1,30 @@
+package com.wizeline.bootcamp.capstone.data.mock
+
+import com.google.gson.annotations.SerializedName
+
+data class AvailableBookResponse (
+    @SerializedName("success") var success : Boolean,
+    @SerializedName("payload") var payload : List<AvailableBook>
+)
+
+data class AvailableBook(
+    val book: String,
+
+    @SerializedName("minimum_amount")
+    val minimumAmount: String,
+
+    @SerializedName("maximum_amount")
+    val maximumAmount: String,
+
+    @SerializedName("minimum_price")
+    val minimumPrice: String,
+
+    @SerializedName("maximum_price")
+    val maximumPrice: String,
+
+    @SerializedName("minimum_value")
+    val minimumValue: String,
+
+    @SerializedName("maximum_value")
+    val maximumValue: String
+)
