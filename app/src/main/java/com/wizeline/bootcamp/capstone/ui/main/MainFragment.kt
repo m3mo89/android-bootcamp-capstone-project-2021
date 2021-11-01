17a96e3125c5bab7c19cdf96e87088779b9601c7
Guillermo Tinoco <m3mo89@gmail.com>
2021-11-01 14:50:59 -0600

    Test AvailableBooks service

--- app/src/main/java/com/wizeline/bootcamp/capstone/ui/main/MainFragment.kt
+++ app/src/main/java/com/wizeline/bootcamp/capstone/ui/main/MainFragment.kt
@@ -26,7 +26,8 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        // Use the ViewModel
+        viewModel.loadData()
     }
 
 }
--- app/src/main/java/com/wizeline/bootcamp/capstone/ui/main/MainViewModel.kt
+++ app/src/main/java/com/wizeline/bootcamp/capstone/ui/main/MainViewModel.kt
@@ -1,7 +1,22 @@
 package com.wizeline.bootcamp.capstone.ui.main
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.wizeline.bootcamp.capstone.data.repo.AvailableBooksRepo
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    fun loadData() {
+        viewModelScope.launch {
+            val repo:AvailableBooksRepo = AvailableBooksRepo()
+            val response = repo.getAvailableBookService().body()
+
+            val payload = response!!.payload
+
+            payload.forEach {
+                println("Book: ${it.book}")
+            }
+
+        }
+    }
 }
