a4304b3f78a333abeb128218e1b1591f6c6e8d2c
Guillermo Tinoco <m3mo89@gmail.com>
2021-11-03 10:42:30 -0600

    Update main activity

--- app/src/main/java/com/wizeline/bootcamp/capstone/MainActivity.kt
+++ app/src/main/java/com/wizeline/bootcamp/capstone/MainActivity.kt
@@ -2,17 +2,21 @@ package com.wizeline.bootcamp.capstone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.wizeline.bootcamp.capstone.ui.main.MainFragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.wizeline.bootcamp.capstone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
