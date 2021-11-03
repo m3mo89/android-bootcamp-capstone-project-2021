b8f37af95d7f77f7c271e4e11bcd3b77858cc28a
Guillermo Tinoco <m3mo89@gmail.com>
2021-11-03 13:30:34 -0600

    Update the book list fragment

--- app/src/main/java/com/wizeline/bootcamp/capstone/ui/booklist/BookListFragment.kt
+++ app/src/main/java/com/wizeline/bootcamp/capstone/ui/booklist/BookListFragment.kt
@@ -6,7 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.wizeline.bootcamp.capstone.R
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.wizeline.bootcamp.capstone.data.mock.mockBooks
+import com.wizeline.bootcamp.capstone.databinding.FragmentBookListBinding
+
+// view holders' height should take 1/7 of the screen
+private const val VIEW_HOLDER_SCREEN_PROPORTION = 1.0 / 7.0
 
 class BookListFragment : Fragment() {
 
@@ -14,13 +22,24 @@ class BookListFragment : Fragment() {
         fun newInstance() = BookListFragment()
     }
 
+    private var _binding: FragmentBookListBinding? = null
+    private val binding: FragmentBookListBinding
+        get() = _binding!!
+
+    private lateinit var navController: NavController
+
+    private lateinit var bookListAdapter: BookListAdapter
+
     private lateinit var viewModel: BookListViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_book_list, container, false)
+        return FragmentBookListBinding
+            .inflate(layoutInflater, container, false)
+            .apply { _binding = this }
+            .root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -29,4 +48,24 @@ class BookListFragment : Fragment() {
         // TODO: Use the ViewModel
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        bookListAdapter = BookListAdapter { bookId ->
+            BookListFragmentDirections
+                .toBookDetailsFragment(bookId)
+                .let { navController.navigate(it) }
+        }
+        navController = findNavController()
+        binding.bookList.run {
+            adapter = bookListAdapter
+            layoutManager = object : LinearLayoutManager(requireContext()) {
+                override fun checkLayoutParams(lp: RecyclerView.LayoutParams?): Boolean {
+                    lp?.height = (height * VIEW_HOLDER_SCREEN_PROPORTION).toInt()
+                    return true
+                }
+            }
+            setHasFixedSize(true)
+        }
+
+        bookListAdapter.submitList(mockBooks)
+    }
 }
