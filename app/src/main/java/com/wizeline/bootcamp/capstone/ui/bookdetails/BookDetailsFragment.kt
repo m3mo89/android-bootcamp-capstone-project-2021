3b7e48c27bd77d67784099cc4c52bf8a63abec1d
Guillermo Tinoco <m3mo89@gmail.com>
2021-11-04 13:50:56 -0600

    Update book details fragment

--- app/src/main/java/com/wizeline/bootcamp/capstone/ui/bookdetails/BookDetailsFragment.kt
+++ app/src/main/java/com/wizeline/bootcamp/capstone/ui/bookdetails/BookDetailsFragment.kt
@@ -6,7 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import com.wizeline.bootcamp.capstone.R
+import com.wizeline.bootcamp.capstone.data.mock.mockAsks
+import com.wizeline.bootcamp.capstone.data.mock.mockBids
+import com.wizeline.bootcamp.capstone.data.mock.mockTicker
+import com.wizeline.bootcamp.capstone.databinding.FragmentBookDetailsBinding
+import com.wizeline.bootcamp.capstone.databinding.FragmentBookListBinding
+import com.wizeline.bootcamp.capstone.ui.booklist.BookListAdapter
 
 class BookDetailsFragment : Fragment() {
 
@@ -14,19 +22,60 @@ class BookDetailsFragment : Fragment() {
         fun newInstance() = BookDetailsFragment()
     }
 
+    private var _binding: FragmentBookDetailsBinding? = null
+    private val binding: FragmentBookDetailsBinding
+        get() = _binding!!
+
+    private lateinit var askListAdapter: AskListAdapter
+
+    private lateinit var bidListAdapter: BidListAdapter
+
     private lateinit var viewModel: BookDetailsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_book_details, container, false)
+        return FragmentBookDetailsBinding
+            .inflate(layoutInflater, container, false)
+            .apply { _binding = this }
+            .root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(BookDetailsViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        askListAdapter = AskListAdapter()
+        bidListAdapter = BidListAdapter()
+
+        binding.askDetailsList.run {
+            adapter = askListAdapter
+            layoutManager = object : LinearLayoutManager(requireContext(), HORIZONTAL, false){
+
+            }
+        }
+
+        binding.bidDetailsList.run {
+            adapter = bidListAdapter
+            layoutManager = object : LinearLayoutManager(requireContext(), HORIZONTAL, false){
+
+            }
+        }
+
+        askListAdapter.submitList(mockAsks)
+        bidListAdapter.submitList(mockBids)
+
+        binding.book.text = mockTicker.id
+        binding.cryptoName.text = mockTicker.cryptoName
+        binding.bookPrice.text = mockTicker.lastPrice
+        binding.dayHighLow.text = "${mockTicker.lowPrice} - ${mockTicker.highPrice}"
+        binding.askPriceValue.text = mockTicker.ask
+        binding.bidPriceValue.text = mockTicker.bid
+        Glide.with(binding.root).load(mockTicker.spriteUrl).into(binding.bookSpriteUrl)
     }
 
 }
