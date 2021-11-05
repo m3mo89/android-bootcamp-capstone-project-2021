4e94776ccdd7f0536cb01f8ab98bf6b6a2d5a875
Guillermo Tinoco <m3mo89@gmail.com>
2021-11-05 17:06:10 -0600

    Implement the book details view model

--- app/src/main/java/com/wizeline/bootcamp/capstone/ui/bookdetails/BookDetailsViewModel.kt
+++ app/src/main/java/com/wizeline/bootcamp/capstone/ui/bookdetails/BookDetailsViewModel.kt
@@ -1,7 +1,76 @@
 package com.wizeline.bootcamp.capstone.ui.bookdetails
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.wizeline.bootcamp.capstone.data.mapper.OrderBookAskResponseMapper
+import com.wizeline.bootcamp.capstone.data.mapper.OrderBookBidResponseMapper
+import com.wizeline.bootcamp.capstone.data.mapper.TickerResponseMapper
+import com.wizeline.bootcamp.capstone.data.repo.OrderBookRepo
+import com.wizeline.bootcamp.capstone.data.repo.TickerRepo
+import com.wizeline.bootcamp.capstone.di.NetworkingModule
+import com.wizeline.bootcamp.capstone.domain.Ask
+import com.wizeline.bootcamp.capstone.domain.Bid
+import com.wizeline.bootcamp.capstone.domain.Ticker
+import kotlinx.coroutines.launch
 
 class BookDetailsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val retrofitClient = NetworkingModule.provideRetrofitClient()
+    private val tickerService = NetworkingModule.provideTickerService(retrofitClient)
+    private val orderBookService = NetworkingModule.provideOrderBookService(retrofitClient)
+
+    private var _ticker: MutableLiveData<Ticker?> = MutableLiveData<Ticker?>()
+    val ticker: LiveData<Ticker?> = _ticker
+
+    private var _askList: MutableLiveData<List<Ask>?> = MutableLiveData<List<Ask>?>()
+    val askList: LiveData<List<Ask>?> = _askList
+
+    private var _bidList: MutableLiveData<List<Bid>?> = MutableLiveData<List<Bid>?>()
+    val bidList: LiveData<List<Bid>?> = _bidList
+
+    fun requestData(book: String) {
+        viewModelScope.launch {
+            val repo: TickerRepo = TickerRepo(tickerService)
+            val tickerMapper = TickerResponseMapper()
+            val response = repo.getTickerByBook(book).body()
+
+            val success = response?.success ?: false
+
+            if (success) {
+                val payload = response?.payload
+
+                if (payload != null) {
+                    val ticker = tickerMapper.map(payload)
+
+                    _ticker.postValue(ticker)
+                }
+            }
+        }
+    }
+
+    fun requestOrderBookData(book: String) {
+        viewModelScope.launch {
+            val orderbookAskMapper = OrderBookAskResponseMapper()
+            val orderbookBidMapper = OrderBookBidResponseMapper()
+            val repo: OrderBookRepo = OrderBookRepo(orderBookService)
+            val response = repo.getOrderBookByBookAndAggregate(book, true).body()
+
+            val success = response?.success ?: false
+
+            if (success) {
+                val payload = response?.payload
+
+                if (payload != null) {
+                    val asks = orderbookAskMapper.mapList(payload.asks)
+
+                    val bids = orderbookBidMapper.mapList(payload.bids)
+
+                    _askList.postValue(asks)
+
+                    _bidList.postValue(bids)
+                }
+            }
+        }
+    }
 }
