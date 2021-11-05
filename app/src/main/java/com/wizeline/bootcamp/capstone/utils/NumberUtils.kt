6bc4e45dc451c7fb74124439aea4305d2942a10a
Guillermo Tinoco <m3mo89@gmail.com>
2021-11-05 16:52:41 -0600

    Format number as currency

---
+++ app/src/main/java/com/wizeline/bootcamp/capstone/utils/NumberUtils.kt
@@ -0,0 +1,12 @@
+package com.wizeline.bootcamp.capstone.utils
+
+import java.text.NumberFormat
+import java.util.Locale
+
+fun Double.asPrice(): String = NumberFormat.getCurrencyInstance(Locale.getDefault()).apply {
+    maximumFractionDigits = if (this@asPrice > 1) {
+        2
+    } else {
+        8
+    }
+}.format(this)
