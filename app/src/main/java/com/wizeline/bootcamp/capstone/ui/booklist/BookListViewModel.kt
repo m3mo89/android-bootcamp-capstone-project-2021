7b4080d033dde8ad056c7c68352f2ace42e74541
Guillermo Tinoco <m3mo89@gmail.com>
2021-11-05 19:35:46 -0600

    Add dependencies to book list view model

--- app/src/main/java/com/wizeline/bootcamp/capstone/ui/booklist/BookListViewModel.kt
+++ app/src/main/java/com/wizeline/bootcamp/capstone/ui/booklist/BookListViewModel.kt
@@ -6,23 +6,19 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.wizeline.bootcamp.capstone.data.mapper.AvailableBookResponseMapper
 import com.wizeline.bootcamp.capstone.data.repo.AvailableBooksRepo
-import com.wizeline.bootcamp.capstone.di.NetworkingModule
 import com.wizeline.bootcamp.capstone.domain.Book
 import kotlinx.coroutines.launch
 
-class BookListViewModel : ViewModel() {
-    private val retrofitClient = NetworkingModule.provideRetrofitClient()
-    private val availableBooksService =
-        NetworkingModule.provideAvailableBooksService(retrofitClient)
-
+class BookListViewModel(
+    private val availableBooksRepo: AvailableBooksRepo,
+    private val booksMapper: AvailableBookResponseMapper,
+) : ViewModel() {
     private var _bookList: MutableLiveData<List<Book>?> = MutableLiveData<List<Book>?>()
     val bookList: LiveData<List<Book>?> = _bookList
 
     fun requestData() {
         viewModelScope.launch {
-            val repo: AvailableBooksRepo = AvailableBooksRepo(availableBooksService)
-            val booksMapper = AvailableBookResponseMapper()
-            val response = repo.getAvailableBooks().body()
+            val response = availableBooksRepo.getAvailableBooks().body()
             val success = response?.success ?: false
 
             if (success) {
