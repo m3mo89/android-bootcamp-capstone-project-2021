d0fe49c44e0a75eb4dd49d77351c7569c1bcb146
Guillermo Tinoco <m3mo89@gmail.com>
2021-11-21 22:11:43 -0800

    Add data access strategy

---
+++ app/src/main/java/com/wizeline/bootcamp/capstone/utils/DataAccessStrategy.kt
@@ -0,0 +1,29 @@
+package com.wizeline.bootcamp.capstone.utils
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
+import androidx.lifecycle.map
+import com.wizeline.bootcamp.capstone.data.NetworkResult
+import kotlinx.coroutines.Dispatchers
+
+fun <T, A> performGetOperation(
+    databaseQuery: () -> LiveData<T>,
+    networkCall: suspend () -> NetworkResult<A>,
+    saveCallResult: suspend (A) -> Unit
+): LiveData<NetworkResult<T>> =
+    liveData(Dispatchers.IO) {
+        emit(NetworkResult.Loading())
+        val source: LiveData<NetworkResult<T>> =
+            databaseQuery.invoke().map { NetworkResult.Success(it) }
+        emitSource(source)
+
+        val responseStatus = networkCall.invoke()
+
+        if (responseStatus is NetworkResult.Success) {
+            saveCallResult(responseStatus.data!!)
+
+        } else if (responseStatus is NetworkResult.Error) {
+            emit(NetworkResult.Error(responseStatus.message!!))
+            emitSource(source)
+        }
+    }
