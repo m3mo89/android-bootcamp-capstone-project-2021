ea14adf685af05a639023c6e207e8201140488f3
Guillermo Tinoco <m3mo89@gmail.com>
2021-11-21 22:33:10 -0800

    Add provides methods

--- app/src/main/java/com/wizeline/bootcamp/capstone/MainActivity.kt
+++ app/src/main/java/com/wizeline/bootcamp/capstone/MainActivity.kt
@@ -6,11 +6,15 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.wizeline.bootcamp.capstone.databinding.ActivityMainBinding
+import com.wizeline.bootcamp.capstone.di.NetworkingModule
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        this.application?.let { NetworkingModule.initializeDI(it) }
+
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
--- app/src/main/java/com/wizeline/bootcamp/capstone/di/NetworkingModule.kt
+++ app/src/main/java/com/wizeline/bootcamp/capstone/di/NetworkingModule.kt
@@ -1,5 +1,10 @@
 package com.wizeline.bootcamp.capstone.di
 
+import android.app.Application
+import com.wizeline.bootcamp.capstone.data.local.AppDatabase
+import com.wizeline.bootcamp.capstone.data.local.BookDAO
+import com.wizeline.bootcamp.capstone.data.remote.BookRemoteDataSource
+import com.wizeline.bootcamp.capstone.data.repo.AvailableBooksRepo
 import com.wizeline.bootcamp.capstone.data.services.AvailableBooksService
 import com.wizeline.bootcamp.capstone.data.services.OrderBookService
 import com.wizeline.bootcamp.capstone.data.services.TickerService
@@ -8,6 +13,17 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object NetworkingModule {
+    @Volatile
+    lateinit var application: Application
+
+    fun initializeDI(_application: Application) {
+        if (!::application.isInitialized) {
+            synchronized(this) {
+                application = _application
+            }
+        }
+    }
+
     fun provideRetrofitClient(): Retrofit {
         return Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
@@ -26,4 +42,19 @@ object NetworkingModule {
     fun provideOrderBookService(retrofit: Retrofit): OrderBookService {
         return retrofit.create(OrderBookService::class.java)
     }
+
+    fun provideDatabase(appContext: Application) =
+        AppDatabase.getDatabase(appContext)
+
+    fun provideBookRemoteDataSource(availableBookService: AvailableBooksService) =
+        BookRemoteDataSource(availableBookService)
+
+    fun provideBookDao(db: AppDatabase) =
+        db.bookDao()
+
+    fun provideRepository(
+        remoteDataSource: BookRemoteDataSource,
+        localDataSource: BookDAO
+    ) =
+        AvailableBooksRepo(remoteDataSource, localDataSource)
 }
