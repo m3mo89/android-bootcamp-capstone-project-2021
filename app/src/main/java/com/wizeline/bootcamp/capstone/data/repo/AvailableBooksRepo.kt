f9e099fd8031f8575883c05751d2e9d4796524b2
Guillermo Tinoco <m3mo89@gmail.com>
2021-11-21 22:19:28 -0800

    Refactor for available books repository

--- app/src/main/java/com/wizeline/bootcamp/capstone/data/repo/AvailableBooksRepo.kt
+++ app/src/main/java/com/wizeline/bootcamp/capstone/data/repo/AvailableBooksRepo.kt
@@ -1,8 +1,32 @@
 package com.wizeline.bootcamp.capstone.data.repo
 
-import com.wizeline.bootcamp.capstone.data.services.AvailableBooksService
+import androidx.lifecycle.map
+import com.wizeline.bootcamp.capstone.data.local.BookDAO
+import com.wizeline.bootcamp.capstone.data.mapper.fromLocal
+import com.wizeline.bootcamp.capstone.data.mapper.toLocal
+import com.wizeline.bootcamp.capstone.data.remote.BookRemoteDataSource
+import com.wizeline.bootcamp.capstone.utils.performGetOperation
 
-class AvailableBooksRepo(private val availableBooksService: AvailableBooksService) {
-    suspend fun getAvailableBooks() =
-        availableBooksService.getAvailableBooks()
+class AvailableBooksRepo(
+    private val remoteDataSource: BookRemoteDataSource,
+    private val localDataSource: BookDAO
+) {
+    fun getAvailableBooks() = performGetOperation(
+        databaseQuery =
+        {
+            localDataSource.getAll().map {
+                it.map { book ->
+                    book.fromLocal()
+                }
+            }
+        },
+        networkCall = { remoteDataSource.getAvailableBooks() },
+        saveCallResult = { it ->
+            val payload = it.payload
+
+            if (payload != null) {
+                localDataSource.insertAll(payload.map { it.toLocal() })
+            }
+        }
+    )
 }
