f8c5bb604ad5e3ffe4e5cd87f1ca9d7cdddb372e
Guillermo Tinoco <m3mo89@gmail.com>
2021-11-23 10:12:53 -0800

    Ticker repository refactor

--- app/src/main/java/com/wizeline/bootcamp/capstone/data/repo/TickerRepo.kt
+++ app/src/main/java/com/wizeline/bootcamp/capstone/data/repo/TickerRepo.kt
@@ -1,8 +1,32 @@
 package com.wizeline.bootcamp.capstone.data.repo
 
-import com.wizeline.bootcamp.capstone.data.services.TickerService
+import androidx.lifecycle.map
+import com.wizeline.bootcamp.capstone.data.local.TickerDAO
+import com.wizeline.bootcamp.capstone.data.mapper.fromLocal
+import com.wizeline.bootcamp.capstone.data.mapper.toLocal
+import com.wizeline.bootcamp.capstone.data.remote.TickerRemoteDataSource
+import com.wizeline.bootcamp.capstone.utils.performGetOperation
 
-class TickerRepo(private val tickerService: TickerService) {
-    suspend fun getTickerByBook(book: String) =
-        tickerService.getTickerByBook(book)
+class TickerRepo(
+    private val remoteDataSource: TickerRemoteDataSource,
+    private val localDataSource: TickerDAO
+) {
+    fun getTickerByBook(book: String) = performGetOperation(
+        databaseQuery =
+        {
+            val ticker = localDataSource.getTickerByBook(book)
+
+            ticker.map {
+                it.let { it.fromLocal() }
+            }
+        },
+        networkCall = { remoteDataSource.getTickerByBook(book) },
+        saveCallResult = { it ->
+            val payload = it.payload
+
+            if (payload != null) {
+                localDataSource.insert(payload.toLocal())
+            }
+        }
+    )
 }
