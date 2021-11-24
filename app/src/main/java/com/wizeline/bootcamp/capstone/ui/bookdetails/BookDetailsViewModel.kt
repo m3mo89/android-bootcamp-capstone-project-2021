e63339aa7762c706e2891b5b2a2e1a32390c7d49
Guillermo Tinoco <m3mo89@gmail.com>
2021-11-24 12:19:03 -0800

    Book details view model refactor

--- app/src/main/java/com/wizeline/bootcamp/capstone/ui/bookdetails/BookDetailsViewModel.kt
+++ app/src/main/java/com/wizeline/bootcamp/capstone/ui/bookdetails/BookDetailsViewModel.kt
@@ -1,58 +1,36 @@
 package com.wizeline.bootcamp.capstone.ui.bookdetails
 
-import androidx.lifecycle.*
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
 import com.wizeline.bootcamp.capstone.data.NetworkResult
-import com.wizeline.bootcamp.capstone.data.mapper.OrderBookResponseMapper
 import com.wizeline.bootcamp.capstone.data.repo.OrderBookRepo
 import com.wizeline.bootcamp.capstone.data.repo.TickerRepo
 import com.wizeline.bootcamp.capstone.domain.OrderBookDTO
 import com.wizeline.bootcamp.capstone.domain.TickerDTO
-import com.wizeline.bootcamp.capstone.utils.Constants.Companion.ERROR_MESSAGE
-import kotlinx.coroutines.launch
 
 class BookDetailsViewModel(
     private val tickerRepo: TickerRepo,
     private val orderBookRepo: OrderBookRepo,
-    private val orderbookMapper: OrderBookResponseMapper,
 ) : ViewModel() {
-    private var _orderBook: MutableLiveData<NetworkResult<OrderBookDTO>> =
-        MutableLiveData<NetworkResult<OrderBookDTO>>()
-    val orderBook: LiveData<NetworkResult<OrderBookDTO>> = _orderBook
+    private val _bookForTicker = MutableLiveData<String>()
+    private val _bookForOrderBook = MutableLiveData<String>()
 
-    private val _book = MutableLiveData<String>()
-
-    private val _ticker = _book.switchMap { book ->
+    private val _ticker = _bookForTicker.switchMap { book ->
         tickerRepo.getTickerByBook(book)
+    }
+
+    private val _orderBook = _bookForOrderBook.switchMap { book ->
+        orderBookRepo.getOrderBookByBook(book)
     }
 
     val ticker: LiveData<NetworkResult<TickerDTO>> = _ticker
 
+    val orderBook: LiveData<NetworkResult<OrderBookDTO>> = _orderBook
+
     fun requestData(book: String) {
-        _book.value = book
-    }
-
-    fun requestOrderBookData(book: String) {
-        viewModelScope.launch {
-            _orderBook.postValue(NetworkResult.Loading())
-
-            try {
-                val response = orderBookRepo.getOrderBookByBookAndAggregate(book, true).body()
-                val success = response?.success ?: false
-
-                if (success) {
-                    val payload = response?.payload
-
-                    if (payload != null) {
-                        val orderBook = orderbookMapper.map(payload)
-
-                        _orderBook.postValue(NetworkResult.Success(orderBook))
-                    }
-                } else {
-                    _orderBook.postValue(NetworkResult.Error(ERROR_MESSAGE))
-                }
-            } catch (e: Exception) {
-                _orderBook.postValue(NetworkResult.Error(e.message))
-            }
-        }
+        _bookForTicker.value = book
+        _bookForOrderBook.value = book
     }
 }
--- app/src/main/java/com/wizeline/bootcamp/capstone/ui/bookdetails/BookDetailsViewModelFactory.kt
+++ app/src/main/java/com/wizeline/bootcamp/capstone/ui/bookdetails/BookDetailsViewModelFactory.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.AbstractSavedStateViewModelFactory
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.savedstate.SavedStateRegistryOwner
-import com.wizeline.bootcamp.capstone.data.mapper.OrderBookResponseMapper
 import com.wizeline.bootcamp.capstone.data.repo.OrderBookRepo
 import com.wizeline.bootcamp.capstone.data.repo.TickerRepo
 
@@ -13,7 +12,6 @@ class BookDetailsViewModelFactory(
     owner: SavedStateRegistryOwner,
     private val tickerRepo: TickerRepo,
     private val orderBookRepo: OrderBookRepo,
-    private val orderbookMapper: OrderBookResponseMapper,
     defaultArgs: Bundle? = null
 ) : AbstractSavedStateViewModelFactory(owner, defaultArgs) {
     override fun <T : ViewModel> create(
@@ -22,7 +20,6 @@ class BookDetailsViewModelFactory(
         return BookDetailsViewModel(
             tickerRepo,
             orderBookRepo,
-            orderbookMapper,
         ) as T
     }
 }
