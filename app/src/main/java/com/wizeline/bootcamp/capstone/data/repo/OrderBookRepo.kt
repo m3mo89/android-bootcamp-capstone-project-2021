543638f5c7b508e76ca41fa463f552d33bb8f575
Guillermo Tinoco <m3mo89@gmail.com>
2021-11-24 12:14:24 -0800

    Refactor for order book repository

--- app/src/main/java/com/wizeline/bootcamp/capstone/data/repo/OrderBookRepo.kt
+++ app/src/main/java/com/wizeline/bootcamp/capstone/data/repo/OrderBookRepo.kt
@@ -1,8 +1,34 @@
 package com.wizeline.bootcamp.capstone.data.repo
 
-import com.wizeline.bootcamp.capstone.data.services.OrderBookService
+import androidx.lifecycle.map
+import com.wizeline.bootcamp.capstone.data.local.OrderBookDAO
+import com.wizeline.bootcamp.capstone.data.mapper.fromLocal
+import com.wizeline.bootcamp.capstone.data.mapper.toLocal
+import com.wizeline.bootcamp.capstone.data.remote.OrderBookRemoteDataSource
+import com.wizeline.bootcamp.capstone.utils.performGetOperation
 
-class OrderBookRepo(private val orderBookService: OrderBookService) {
-    suspend fun getOrderBookByBookAndAggregate(book: String, aggregate: Boolean) =
-        orderBookService.getOrderBookByBookAndAggregate(book, aggregate)
+class OrderBookRepo(
+    private val remoteDataSource: OrderBookRemoteDataSource,
+    private val localDataSource: OrderBookDAO
+) {
+    fun getOrderBookByBook(book: String) = performGetOperation(
+        databaseQuery =
+        {
+            val ordersBook = localDataSource.getOrderBookByBook(book)
+
+            ordersBook.let {
+                ordersBook.map { orderBook ->
+                    orderBook.fromLocal()
+                }
+            }
+        },
+        networkCall = { remoteDataSource.getOrderBookByBook(book) },
+        saveCallResult = { it ->
+            val payload = it.payload
+
+            if (payload != null) {
+                localDataSource.insert(payload.toLocal())
+            }
+        }
+    )
 }
