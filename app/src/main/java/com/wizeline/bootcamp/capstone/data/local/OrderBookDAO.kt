dfe55c44f15de59f25c49f343627208426f011c1
Guillermo Tinoco <m3mo89@gmail.com>
2021-11-24 12:02:46 -0800

    Add order book dao

---
+++ app/src/main/java/com/wizeline/bootcamp/capstone/data/local/OrderBookDAO.kt
@@ -0,0 +1,17 @@
+package com.wizeline.bootcamp.capstone.data.local
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.wizeline.bootcamp.capstone.data.entities.OrderBookEntity
+
+@Dao
+interface OrderBookDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(entity: OrderBookEntity)
+
+    @Query("SELECT * FROM orders WHERE book=:book")
+    fun getOrderBookByBook(book: String): LiveData<OrderBookEntity>
+}
