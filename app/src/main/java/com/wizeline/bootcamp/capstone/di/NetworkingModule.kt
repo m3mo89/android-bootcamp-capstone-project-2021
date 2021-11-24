9d93a4c537cc2eeaf2e269e11c2d2b8ec57860f7
Guillermo Tinoco <m3mo89@gmail.com>
2021-11-24 12:16:43 -0800

    Add methods for order book to the networking module

--- app/src/main/java/com/wizeline/bootcamp/capstone/di/NetworkingModule.kt
+++ app/src/main/java/com/wizeline/bootcamp/capstone/di/NetworkingModule.kt
@@ -3,10 +3,13 @@ package com.wizeline.bootcamp.capstone.di
 import android.app.Application
 import com.wizeline.bootcamp.capstone.data.local.AppDatabase
 import com.wizeline.bootcamp.capstone.data.local.BookDAO
+import com.wizeline.bootcamp.capstone.data.local.OrderBookDAO
 import com.wizeline.bootcamp.capstone.data.local.TickerDAO
 import com.wizeline.bootcamp.capstone.data.remote.BookRemoteDataSource
+import com.wizeline.bootcamp.capstone.data.remote.OrderBookRemoteDataSource
 import com.wizeline.bootcamp.capstone.data.remote.TickerRemoteDataSource
 import com.wizeline.bootcamp.capstone.data.repo.AvailableBooksRepo
+import com.wizeline.bootcamp.capstone.data.repo.OrderBookRepo
 import com.wizeline.bootcamp.capstone.data.repo.TickerRepo
 import com.wizeline.bootcamp.capstone.data.services.AvailableBooksService
 import com.wizeline.bootcamp.capstone.data.services.OrderBookService
@@ -92,4 +95,16 @@ object NetworkingModule {
         localDataSource: TickerDAO
     ) =
         TickerRepo(remoteDataSource, localDataSource)
+
+    fun provideOrderBookRemoteDataSource(orderBookService: OrderBookService) =
+        OrderBookRemoteDataSource(orderBookService)
+
+    fun provideOrderBookDao(db: AppDatabase) =
+        db.orderBookDao()
+
+    fun provideOrderBookRepository(
+        remoteDataSource: OrderBookRemoteDataSource,
+        localDataSource: OrderBookDAO
+    ) =
+        OrderBookRepo(remoteDataSource, localDataSource)
 }
