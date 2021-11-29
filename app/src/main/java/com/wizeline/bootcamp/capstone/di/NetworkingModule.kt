85a000a96d5330dd3ec659494105cb58f38f7c4d
Guillermo Tinoco <m3mo89@gmail.com>
2021-11-29 17:04:40 -0600

    Refactor for networking module

--- app/src/main/java/com/wizeline/bootcamp/capstone/di/NetworkingModule.kt
+++ app/src/main/java/com/wizeline/bootcamp/capstone/di/NetworkingModule.kt
@@ -17,23 +17,21 @@ import com.wizeline.bootcamp.capstone.data.services.TickerService
 import com.wizeline.bootcamp.capstone.utils.Constants
 import com.wizeline.bootcamp.capstone.utils.Constants.Companion.USER_AGENT_KEY
 import com.wizeline.bootcamp.capstone.utils.Constants.Companion.USER_AGENT_VALUE
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkingModule {
-    @Volatile
-    lateinit var application: Application
-
-    fun initializeDI(_application: Application) {
-        if (!::application.isInitialized) {
-            synchronized(this) {
-                application = _application
-            }
-        }
-    }
-
+    @Provides
+    @Singleton
     fun provideRetrofitClient(): Retrofit {
         return Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
@@ -57,51 +55,77 @@ object NetworkingModule {
             .build()
     }
 
+    @Provides
+    @Singleton
     fun provideAvailableBooksService(retrofit: Retrofit): AvailableBooksService {
         return retrofit.create(AvailableBooksService::class.java)
     }
 
+    @Provides
+    @Singleton
     fun provideTickerService(retrofit: Retrofit): TickerService {
         return retrofit.create(TickerService::class.java)
     }
 
+    @Provides
+    @Singleton
     fun provideOrderBookService(retrofit: Retrofit): OrderBookService {
         return retrofit.create(OrderBookService::class.java)
     }
 
+    @Provides
+    @Singleton
     fun provideDatabase(appContext: Application) =
         AppDatabase.getDatabase(appContext)
 
+    @Provides
+    @Singleton
     fun provideBookRemoteDataSource(availableBookService: AvailableBooksService) =
         BookRemoteDataSource(availableBookService)
 
+    @Provides
+    @Singleton
     fun provideBookDao(db: AppDatabase) =
         db.bookDao()
 
+    @Provides
+    @Singleton
     fun provideAvailableBooksRepository(
         remoteDataSource: BookRemoteDataSource,
         localDataSource: BookDAO
     ) =
         AvailableBooksRepo(remoteDataSource, localDataSource)
 
+    @Provides
+    @Singleton
     fun provideTickerRemoteDataSource(tickerService: TickerService) =
         TickerRemoteDataSource(tickerService)
 
+    @Provides
+    @Singleton
     fun provideTickerDao(db: AppDatabase) =
         db.tickerDao()
 
+    @Provides
+    @Singleton
     fun provideTickerRepository(
         remoteDataSource: TickerRemoteDataSource,
         localDataSource: TickerDAO
     ) =
         TickerRepo(remoteDataSource, localDataSource)
 
+    @Provides
+    @Singleton
     fun provideOrderBookRemoteDataSource(orderBookService: OrderBookService) =
         OrderBookRemoteDataSource(orderBookService)
 
+    @Provides
+    @Singleton
     fun provideOrderBookDao(db: AppDatabase) =
         db.orderBookDao()
 
+    @Provides
+    @Singleton
     fun provideOrderBookRepository(
         remoteDataSource: OrderBookRemoteDataSource,
         localDataSource: OrderBookDAO
