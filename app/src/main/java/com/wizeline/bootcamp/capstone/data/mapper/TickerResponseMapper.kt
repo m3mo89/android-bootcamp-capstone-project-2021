a6a27d4dc94725aef349ebd4ced85c4991ac3d44
Guillermo Tinoco <m3mo89@gmail.com>
2021-12-01 13:06:44 -0600

    Refactor for mappers

--- app/src/main/java/com/wizeline/bootcamp/capstone/data/mapper/TickerResponseMapper.kt
+++ app/src/main/java/com/wizeline/bootcamp/capstone/data/mapper/TickerResponseMapper.kt
@@ -5,6 +5,27 @@ import com.wizeline.bootcamp.capstone.data.mock.Ticker
 import com.wizeline.bootcamp.capstone.domain.TickerDTO
 import com.wizeline.bootcamp.capstone.utils.Constants
 import com.wizeline.bootcamp.capstone.utils.asPrice
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class TickerResponseMapper @Inject constructor() : Mapper<TickerEntity, TickerDTO> {
+    override fun map(from: TickerEntity): TickerDTO {
+        val spriteUrl =
+            "${Constants.CRYPTO_IMAGES_URL}${from.book.substringBefore(Constants.UNDERSCORE_DELIMITER)}"
+
+        return TickerDTO(
+            id = from.book,
+            spriteUrl = spriteUrl,
+            cryptoName = from.book,
+            lastPrice = from.last.asPrice(),
+            highPrice = from.high.asPrice(),
+            lowPrice = from.low.asPrice(),
+            ask = from.ask.asPrice(),
+            bid = from.bid.asPrice()
+        )
+    }
+}
 
 fun Ticker.toLocal() = TickerEntity(
     book = book,
@@ -16,6 +37,19 @@ fun Ticker.toLocal() = TickerEntity(
     ask = ask,
     bid = bid,
     createdAt = createdAt
+)
+
+fun Ticker.toDTO() = TickerDTO(
+    id = book,
+    cryptoName = book,
+    spriteUrl = book.let {
+        "${Constants.CRYPTO_IMAGES_URL}${book.substringBefore(Constants.UNDERSCORE_DELIMITER)}"
+    },
+    lastPrice = last.let { last.asPrice() },
+    highPrice = high.let { high.asPrice() },
+    lowPrice = low.let { low.asPrice() },
+    ask = ask.let { ask.asPrice() },
+    bid = bid.let { bid.asPrice() }
 )
 
 fun TickerEntity.fromLocal() = TickerDTO(
