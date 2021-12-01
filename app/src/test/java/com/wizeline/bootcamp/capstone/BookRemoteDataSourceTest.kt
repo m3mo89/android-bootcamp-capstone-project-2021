0abb30458bd2f8bd466f9fa8c678b81e79915042
Guillermo Tinoco <m3mo89@gmail.com>
2021-12-01 17:21:56 -0600

    Format code

--- app/src/test/java/com/wizeline/bootcamp/capstone/BookRemoteDataSourceTest.kt
+++ app/src/test/java/com/wizeline/bootcamp/capstone/BookRemoteDataSourceTest.kt
@@ -6,26 +6,29 @@ import com.wizeline.bootcamp.capstone.data.remote.BookRemoteDataSource
 import com.wizeline.bootcamp.capstone.data.services.AvailableBooksService
 import io.reactivex.Observable
 import io.reactivex.observers.TestObserver
-import org.junit.Assert.assertFalse
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
-import org.mockito.Mockito
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
 
 class BookRemoteDataSourceTest {
     private lateinit var bookRemoteDataSource: BookRemoteDataSource
+
     @Mock
     private lateinit var availableBooksService: AvailableBooksService
     private val books = listOf(
-        AvailableBook(book = "btc_mxn", minimumAmount = ".003",
+        AvailableBook(
+            book = "btc_mxn", minimumAmount = ".003",
             maximumAmount = "1000.00", minimumPrice = "100.00",
-            maximumPrice = "1000000.00", minimumValue = "25.00", maximumValue = "1000000.00"),
-        AvailableBook(book = "eth_mxn", minimumAmount = ".003",
+            maximumPrice = "1000000.00", minimumValue = "25.00", maximumValue = "1000000.00"
+        ),
+        AvailableBook(
+            book = "eth_mxn", minimumAmount = ".003",
             maximumAmount = "1000.00", minimumPrice = "100.00",
-            maximumPrice = "1000000.00", minimumValue = "25.00", maximumValue = "1000000.00"),
+            maximumPrice = "1000000.00", minimumValue = "25.00", maximumValue = "1000000.00"
+        ),
     )
 
     private val response = AvailableBooksResponse(success = true, payload = books)
