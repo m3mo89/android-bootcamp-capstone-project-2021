fb27e1f6572b2a4dec280c3a1b2506860ef79832
Guillermo Tinoco <m3mo89@gmail.com>
2021-12-01 18:38:01 -0600

    Missing imports

--- app/src/androidTest/java/com/wizeline/bootcamp/capstone/MainActivityTest.kt
+++ app/src/androidTest/java/com/wizeline/bootcamp/capstone/MainActivityTest.kt
@@ -2,6 +2,8 @@ package com.wizeline.bootcamp.capstone
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
